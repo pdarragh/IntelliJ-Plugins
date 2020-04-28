817888d0c6e6b16361fb6673462eab7e28734934
Pierce Darragh <pierce.darragh@gmail.com>
2020-04-28 17:11:43 -0600

    Add in-progress work that never got added

---
+++ viper-grammar/src/com/vgfplugin/psi/impl/VgfPsiImplUtil.java
@@ -0,0 +1,14 @@
+package com.vgfplugin.psi.impl;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.navigation.ItemPresentation;
+import com.intellij.psi.*;
+import com.vgfplugin.VgfIcons;
+import com.vgfplugin.psi.*;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class VgfPsiImplUtil {
+
+}
